b9b2fb31049aa5618d6f402a68722ef9bd006c92
Alex Kroman <alex@opensourcery.com>
2007-10-05 17:00:51 +0000

    updating environment file

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 # Specifies order to load fixtures to prevent foreign key problems
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '1.2.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
