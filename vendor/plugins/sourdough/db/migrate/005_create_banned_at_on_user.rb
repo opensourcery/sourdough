ce1ad74c418effab5ccf2708f012ec937b33f481
Alex Kroman <alex@opensourcery.com>
2007-10-15 23:22:56 +0000

    revert last change

--- vendor/plugins/sourdough/db/migrate/005_create_banned_at_on_user.rb
+++ vendor/plugins/sourdough/db/migrate/005_create_banned_at_on_user.rb
@@ -1,10 +1,14 @@
 # This file is part of Sourdough.  Copyright 2006,2007 OpenSourcery, LLC.  This program is free software, licensed under the terms of the GNU General Public License.  Please see the COPYING file in this distribution for more information, or see http://www.gnu.org/copyleft/gpl.html.
 class CreateBannedAtOnUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :banned_at, :datetime
+    User.transaction do 
+      add_column :users, :banned_at, :datetime
+    end
   end
 
   def self.down
-    remove_column :users, :banned_at
+    User.transaction do
+      remove_column :users, :banned_at
+    end
   end
 end
