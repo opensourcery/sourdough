094985568e1328f1a670af751105eec2c59fee28
Alex Kroman <alex@opensourcery.com>
2007-12-17 21:25:53 +0000

    basic code coverage test for engine based plugins.. still not wroking for integration tests but it's a start

--- vendor/plugins/sourdough/test/integration/spider_test.rb
+++ vendor/plugins/sourdough/test/integration/spider_test.rb
@@ -1,8 +1,8 @@
 # This file is part of Sourdough.  Copyright 2006,2007 OpenSourcery, LLC.  This program is free software, licensed under the terms of the GNU General Public License.  Please see the COPYING file in this distribution for more information, or see http://www.gnu.org/copyleft/gpl.html.
-require "#{File.dirname(__FILE__)}/../test_helper"
+require File.dirname(__FILE__) + '/../test_helper'
 
 class SpiderTest < ActionController::IntegrationTest
-  fixtures :users
+  fixtures :users, :photos, :roles_users, :roles
 
   include Caboose::SpiderIntegrator
 
