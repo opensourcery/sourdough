a8691f23a3d25f29b3a1ea6eed260425c8667ea5
Alex Kroman <alex@opensourcery.com>
2008-01-12 23:57:12 +0000

    rename attachable migration

--- vendor/plugins/sourdough/db/migrate/006_create_attachable.rb
+++ vendor/plugins/sourdough/db/migrate/006_create_attachable.rb
@@ -1,5 +1,5 @@
 # This file is part of Sourdough.  Copyright 2006,2007 OpenSourcery, LLC.  This program is free software, licensed under the terms of the GNU General Public License.  Please see the COPYING file in this distribution for more information, or see http://www.gnu.org/copyleft/gpl.html.
-class CreateBannedAtOnUser < ActiveRecord::Migration
+class CreateAttachable < ActiveRecord::Migration
   def self.up
     Photo.transaction do 
       add_column :photos, :attachable_type, :string
