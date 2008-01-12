bb8afeca27e537178ca5fab778fe5eb8eacad352
Alex Kroman <alex@opensourcery.com>
2008-01-12 17:43:03 +0000

    user refactor, abstract out a method so we don't *have* to do email activation

--- vendor/plugins/sourdough/app/controllers/users_controller.rb
+++ vendor/plugins/sourdough/app/controllers/users_controller.rb
@@ -6,53 +6,35 @@ class UsersController < ApplicationController
   before_filter :load_user, :except => [ :index, :create, :new, :activate ]
   before_filter :check_auth, :only => @protected_actions
 
-  # GET /users/1
-  # GET /users/1.xml
   def show
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml  { render :xml => @user.to_xml }
-    end
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1;edit
   def edit
   end
 
-  # POST /users
-  # POST /users.xml
   def create
     @user = User.new(params[:user])
 
     respond_to do |format|
-      if @user.save
-        flash[:notice] = "Thank you for signing up!  We just sent you a confirmation email to #{@user.email}.  Please click on the activation link in the email to complete your sign up."[:user_created]
-        UserMailer.deliver_signup_notification(@user, request.protocol + request.host_with_port + activate_path(@user.activation_code))
+      if @user.save and after_user_save
         format.html { redirect_to create_redirection_path }
-        format.xml  { head :created, :location => home_path }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors.to_xml }
       end
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
   def update
     respond_to do |format|
       if @user.update_attributes(params[:user])
         flash[:notice] = 'User was successfully updated.'[:user_updated]
         format.html { redirect_to update_redirection_path(@user) }
-        format.xml  { head :ok }
       else
         format.html { render :action => update_render_action }
-        format.xml  { render :xml => @user.errors.to_xml }
       end
     end
   end
@@ -68,6 +50,11 @@ class UsersController < ApplicationController
   end
 
   protected
+
+  def after_user_save
+    flash[:notice] = "Thank you for signing up!  We just sent you a confirmation email to #{@user.email}.  Please click on the activation link in the email to complete your sign up."[:user_created]
+    UserMailer.deliver_signup_notification(@user, request.protocol + request.host_with_port + activate_path(@user.activation_code))
+  end
 
   def load_user
     @user = User.find_by_param(params[:id]) or raise ActiveRecord::RecordNotFound
