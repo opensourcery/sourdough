da383305bfeaa2a6fa53afb7aeb405df825591d7
Alex Kroman <alex@opensourcery.com>
2008-03-05 21:48:58 +0000

    make sure we reload plugins in development mode

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
 
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
+  config.plugins = [:engines, :sourdough, :all]
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
