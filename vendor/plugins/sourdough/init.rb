35972b8c0341a8a95fefc629b6238e4965011562
Alex Kroman <alex@opensourcery.com>
2008-09-10 02:50:55 +0000

    Adding load paths back into sourdough

--- vendor/plugins/sourdough/init.rb
+++ vendor/plugins/sourdough/init.rb
@@ -1,3 +1,6 @@
 # This file is part of Sourdough.  Copyright 2006,2007 OpenSourcery, LLC.  This program is free software, licensed under the terms of the GNU General Public License.  Please see the COPYING file in this distribution for more information, or see http://www.gnu.org/copyleft/gpl.html.
 # Include hook code here
 require 'sourdough'
+load_paths.each do |path|
+  Dependencies.load_once_paths.delete(path)
+end
